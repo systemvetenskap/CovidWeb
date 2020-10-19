26c9cd2d5dc6ecf5b20ed40ab08c2e0092481441
Erik Öberg <erik.oberg@miun.se>
2020-10-19 19:22:52 +0200

    Menu ViewComponent

---
+++ CovidWeb.FL2/ViewComponents/MenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CovidWeb.ViewComponents
+{
+    public class MenuViewComponent : ViewComponent
+    {
+        public MenuViewComponent()
+        {
+
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync(string activePage)
+        {
+            await Task.Delay(0);
+            return View("Default");
+        }
+    }
+}
